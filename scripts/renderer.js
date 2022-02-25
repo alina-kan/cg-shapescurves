24fad2133cdfec49a90755a864d94c4dea678ac0
alina-kan <alinak10971@gmail.com>
2022-02-24 22:51:58 -0600

    Update renderer.js

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -47,7 +47,7 @@ class Renderer {
     // ctx:          canvas context
     drawSlide0(ctx) {
         //draw rectangle here
-        drawRectangle((0,0), (400, 500), [0, 0, 255, 255], ctx);
+        drawRectangle((200,200), (400, 400), [0, 0, 255, 255], ctx);
     }
 
     // ctx:          canvas context
