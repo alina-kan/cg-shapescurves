304b251107e31ae8817b1d5be0e4d98009ef1d7e
alina-kan <alinak10971@gmail.com>
2022-03-02 16:17:04 -0600

    drawPointData added
    Makes it easier to draw point data for lines + rectangles

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -74,17 +74,44 @@ class Renderer {
         this.drawLine({x: 175, y: 400}, {x: 250, y: 100}, [151, 82, 168, 255], ctx);
         this.drawLine({x: 137, y: 250}, {x: 212, y: 250}, [151, 82, 168, 255], ctx);
 
+        //all line points for A
+        let points = [];
+        let pt1 = {x: 100, y: 100};
+        let pt2 = {x: 175, y: 400};
+        let pt3 = {x: 250, y: 100};
+        let pt4 = {x: 137, y: 250};
+        let pt5 = {x: 212, y: 250};
+        points.push(pt1, pt2, pt3, pt4, pt5);
+
         //l
         this.drawLine({x: 275, y: 350}, {x: 275, y: 100}, [151, 82, 168, 255], ctx);
+
+        //line points for l
+        let pt6 = {x: 275, y: 350};
+        let pt7 = {x: 275, y: 100};
+        points.push(pt6, pt7);
 
         //i
         this.drawLine({x: 300, y: 250}, {x: 300, y: 100}, [151, 82, 168, 255], ctx);
         this.drawCircle({x: 300, y: 275}, 3, [151, 82, 168, 255], ctx);
 
+        //line points for i
+
+        let pt8 = {x: 300, y: 250};
+        let pt9 = {x: 300, y: 100};
+        points.push(pt8, pt9);
+
         //n
         this.drawLine({x: 325, y: 250}, {x: 325, y: 100}, [151, 82, 168, 255], ctx);
         this.drawBezierCurve({x: 325, y: 200}, {x: 325, y: 270}, {x: 400, y: 270}, {x: 400, y: 200}, [151, 82, 168, 255], ctx);
         this.drawLine({x: 400, y: 200}, {x: 400, y: 100}, [151, 82, 168, 255], ctx);
+
+        //line points for n
+        let pt10 = {x: 325, y: 250};
+        let pt11 = {x: 325, y: 100};
+        let pt12 = {x: 400, y: 200};
+        let pt13 = {x: 400, y: 100};
+        points.push(pt10, pt11, pt12, pt13);
 
         //a - most complicated letter :/
         this.drawBezierCurve({x: 475, y: 250}, {x: 400, y: 260}, {x: 400, y: 50}, {x: 487, y: 110}, [151, 82, 168, 255], ctx);
@@ -92,6 +119,26 @@ class Renderer {
         this.drawLine({x: 487, y: 110}, {x: 498, y: 120}, [151, 82, 168, 255], ctx);
         this.drawLine({x: 498, y: 240}, {x: 498, y: 120}, [151, 82, 168, 255], ctx);
         this.drawBezierCurve({x: 498, y: 120}, {x: 498, y: 100}, {x: 525, y: 100}, {x: 525, y: 105}, [151, 82, 168, 255], ctx);
+
+        //line points for a
+        let pt14 = {x: 475, y: 250};
+        let pt15 = {x: 498, y: 230};
+        let pt16 = {x: 487, y: 110};
+        let pt17 = {x: 498, y: 120};
+        let pt18 = {x: 498, y: 240};
+        let pt19 = {x: 498, y: 120};
+        points.push(pt14, pt15, pt16, pt17, pt18, pt19);
+
+        if (this.show_points){
+            this.show_points = false;
+            for (let i = 0; i < points.length; i++){
+                let pt_x = points[i].x;
+                let pt_y = points[i].y;
+                this.drawPointData({x: pt_x-3, y: pt_y-3}, {x: pt_x+3, y: pt_y+3}, [255, 0, 0, 255], ctx);
+                this.show_points = true;
+            }
+            this.show_points = true;
+        }
 
     }
 
@@ -108,6 +155,20 @@ class Renderer {
         this.drawLine(left_top, right_top, color, ctx);
         this.drawLine(right_top, right_bottom, color, ctx);
         this.drawLine(right_bottom, left_bottom, color, ctx);
+
+        let points = [];
+        points.push(left_bottom, left_top, right_bottom, right_top);
+
+        if (this.show_points){
+            this.show_points = false;
+            for (let i = 0; i < 4; i++){
+                let pt_x = points[i].x;
+                let pt_y = points[i].y;
+                this.drawPointData({x: pt_x-3, y: pt_y-3}, {x: pt_x+3, y: pt_y+3}, [255, 0, 0, 255], ctx);
+                this.show_points = true;
+            }
+            this.show_points = true;
+        }
     }
 
     // center:       object ({x: __, y: __})
@@ -146,7 +207,7 @@ class Renderer {
             for (let i = 0; i < pt_holder.length; i++){
                 let pt_x = pt_holder[i].x;
                 let pt_y = pt_holder[i].y;
-                this.drawRectangle({x: pt_x-3, y: pt_y-3}, {x: pt_x+3, y: pt_y+3}, [255, 0, 0, 255], ctx);
+                this.drawPointData({x: pt_x-3, y: pt_y-3}, {x: pt_x+3, y: pt_y+3}, [255, 0, 0, 255], ctx);
                 this.show_points = true;
             }
         }
@@ -196,18 +257,30 @@ class Renderer {
         if (this.show_points){
             this.show_points = false;
             //control points
-            this.drawRectangle({x: pt1.x-3, y: pt1.y-3}, {x: pt1.x+3, y: pt1.y+3}, [45, 194, 37, 255], ctx);
-            this.drawRectangle({x: pt2.x-3, y: pt2.y-3}, {x: pt2.x+3, y: pt2.y+3}, [45, 194, 37, 255], ctx);
+            this.drawPointData({x: pt1.x-3, y: pt1.y-3}, {x: pt1.x+3, y: pt1.y+3}, [45, 194, 37, 255], ctx);
+            this.drawPointData({x: pt2.x-3, y: pt2.y-3}, {x: pt2.x+3, y: pt2.y+3}, [45, 194, 37, 255], ctx);
             for (let i = 0; i < pt_holder.length; i++){
                 let pt_x = pt_holder[i].x;
                 let pt_y = pt_holder[i].y;
                 //curve points
-                this.drawRectangle({x: pt_x-3, y: pt_y-3}, {x: pt_x+3, y: pt_y+3}, [255, 0, 0, 255], ctx);
+                this.drawPointData({x: pt_x-3, y: pt_y-3}, {x: pt_x+3, y: pt_y+3}, [255, 0, 0, 255], ctx);
                 //setting back to true will allow for the points to continue
                 // being shown when slider moves
                 this.show_points = true;
             }
         }
+    }
+    // pt0:          object ({x: __, y: __})
+    // pt0:          object ({x: __, y: __})
+        // pt0:          object ({x: __, y: __})
+
+    drawPointData(pt_1, pt_2, color, ctx){
+        let pt_3 = new Object({x: pt_1.x, y: pt_2.y});
+        let pt_4 = new Object({x: pt_2.x, y: pt_1.y});
+        this.drawLine(pt_1, pt_3, color, ctx);
+        this.drawLine(pt_3, pt_2, color, ctx);
+        this.drawLine(pt_2, pt_4, color, ctx);
+        this.drawLine(pt_4, pt_1, color, ctx);
     }
 
     // pt0:          object ({x: __, y: __})
